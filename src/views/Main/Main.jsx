270bdb53f8107fc8377fedf1f98877597c9ab7e6
csnark <csnark.dev@gmail.com>
2023-02-21 18:11:04 +0100

    Main view completed

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,12 +1,36 @@
+import './Main.css'
+
 import Header from "../../components/Header/Header"
+import Converter from "../../components/Converter/Converter"
 import Footer from "../../components/Footer/Footer"
 
 const Main = () => {
+    const saved = [
+        "100 miles es mazo",
+        "rubu",
+        "Siluro",
+        "Bravo Murillo"
+    ]
+
     return (
     <>
         <Header />
         <main>
-            
+            <Converter />
+            <section className="saved">
+                <h3>saved</h3>
+                <div className="saved-entries">
+                {saved &&
+                    saved.map((entry, i) => {
+                    return (
+                        <div className="entry">
+                            <p>{entry}</p>
+                            <i className="fa-solid fa-xmark"></i>
+                        </div>
+                    )
+                })}
+                </div>
+            </section>
         </main>
         <Footer />      
     </>
