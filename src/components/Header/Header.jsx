2f53f7eaad57700e64483cd9fe4c35b170e43e33
csnark <csnark.dev@gmail.com>
2023-02-23 22:07:49 +0100

    Header component desktop style completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,10 @@ import './Header.css'
 const Header = () => {
     return (
         <header className="header">  
-            <i className="fa-solid fa-arrow-right-arrow-left"></i>
-            <h1>unit converter</h1>
+            <header className="header-container">
+                <i className="fa-solid fa-arrow-right-arrow-left"></i>
+                <h1>unit converter</h1>
+            </header>
         </header>
     )
 }
